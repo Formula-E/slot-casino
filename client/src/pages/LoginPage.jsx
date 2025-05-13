ba40a6b0f0920bd39c1216b5fe420426a064b874
Formula-E <formulaetrading@gmail.com>
2025-05-13 06:38:59 +0200

    Delete client/src directory

--- client/src/pages/LoginPage.jsx
+++
@@ -1,8 +0,0 @@
-
-import React from "react";
-
-function LoginPage() {
-  return <div className="text-center py-10">LoginPage Page</div>;
-}
-
-export default LoginPage;
--- client/src/pages/TelegramPage.jsx
+++
@@ -1,8 +0,0 @@
-
-import React from "react";
-
-function TelegramPage() {
-  return <div className="text-center py-10">TelegramPage Page</div>;
-}
-
-export default TelegramPage;
