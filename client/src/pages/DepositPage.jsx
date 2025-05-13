6772e8160c3320a2136ecd9d4b22b166ce335307
Formula-E <formulaetrading@gmail.com>
2025-05-13 07:39:03 +0200

    Delete client/src/pages directory

--- client/src/pages/DepositPage.jsx
+++
@@ -1,8 +0,0 @@
-
-import React from "react";
-
-function DepositPage() {
-  return <div className="text-center py-10">DepositPage Page</div>;
-}
-
-export default DepositPage;
--- client/src/pages/ProfilePage.jsx
+++
@@ -1,8 +0,0 @@
-
-import React from "react";
-
-function ProfilePage() {
-  return <div className="text-center py-10">ProfilePage Page</div>;
-}
-
-export default ProfilePage;
