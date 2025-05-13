5b68912b3a29bb459e6312781c5d59c7d3dadde4
Formula-E <formulaetrading@gmail.com>
2025-05-13 06:58:39 +0200

    Updates

---
+++ client/src/pages/DepositPage.jsx
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+function DepositPage() {
+  return <div className="text-center py-10">DepositPage Page</div>;
+}
+
+export default DepositPage;
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+function RegisterPage() {
+  return <div className="text-center py-10">RegisterPage Page</div>;
+}
+
+export default RegisterPage;
